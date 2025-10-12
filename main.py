a67462e794e84b2ebc46c906b7b380cff287ef05
Abhiiii S <bestabhinav97@gmail.com>
2025-10-13 01:32:04 +0530

    Code according to the menu number

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pig.game import Game
 def displayMenu():
     print("1.Play Game")
     print("2.Show statistics")
+    print("3.Cheat Mode")
+    print("4. Display Rules of the Game")
+    
     
 while True:
     displayMenu()
@@ -15,6 +18,10 @@ while True:
         game.playGame()
     elif choice == "2":
         print("Statistics - to be implemented")
+    elif choice == "3":
+        print("Running in cheat mode")
+        game = Game()
+        game.cheatMode()
     elif choice == "0":
         print("EXITING")
         break
--- pig/game.py
+++ pig/game.py
@@ -8,7 +8,17 @@ class Game():
 
         
 
+    def checkRolls(self,die1,die2):
+        if die1 and die2 != 1:
+            score = die1 + die2
+            return score
+        elif die1 == 1 or die2 == 1:
+            return 1
+        elif die1 == 1 and die1 == 1:
+            return 2
     
+    
+
     
     def playGame(self):
         print("PLAY AGAINST FRIEND OR COMPUTER")
@@ -17,6 +27,7 @@ class Game():
 
         gameChoice = input("Enter your choice")
         currentPlayer = 0
+        dice = Dice()
 
         if gameChoice == 1:
             print("Enter player 1 name")
@@ -27,14 +38,27 @@ class Game():
             player2 = Player(p2Name)
 
             
+        gameOver = False
+        while gameOver == False:
+            players = [player1,player2]
+            currentPlayer = 0
+            print(f"{Player[currentPlayer]}'s TURN")
+            die1 = dice.roll()
+            die2 = dice.roll()
+            
+            checkValue = self.checkRolls(die1,die2)
+            if checkValue == 1:
+                players[currentPlayer].runningScore = 0
+                currentPlayer = 1-currentPlayer
+            elif checkValue == 2:
+                players[currentPlayer].runningScore = 0
+                players[currentPlayer].totalScore = 0
+                currentPlayer = 1-currentPlayer
+            else:
+                players[currentPlayer].runningScore = checkValue
+                repeatChoice = input("HOLD OR AGAIN")
 
-            print(f"Player {currentPlayer+1}'s turn")
-            die1 = Dice.roll()
-            die2 = Dice.roll()
 
-            if die1 != 1 and die2!=1:
-                player1.runningScore = die1 + die2
-                print("HOLD or ")            
 
 
 
